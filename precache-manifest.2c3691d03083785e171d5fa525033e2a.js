01b1be33c1b2b8fe61da0e93281355bfd113b173
Jack <jamieson.jack@gmail.com>
2019-06-23 21:54:10 -0400

    Updates

---
+++ precache-manifest.2c3691d03083785e171d5fa525033e2a.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bf776af4e1fb33bb1d7b830ad216a4e5",
+    "url": "/react-clock-radio/index.html"
+  },
+  {
+    "revision": "229fd95a0f56959b4018",
+    "url": "/react-clock-radio/static/css/main.96ab95a3.chunk.css"
+  },
+  {
+    "revision": "08731324183367807ea9",
+    "url": "/react-clock-radio/static/js/2.267e8e58.chunk.js"
+  },
+  {
+    "revision": "229fd95a0f56959b4018",
+    "url": "/react-clock-radio/static/js/main.e25b5a5c.chunk.js"
+  },
+  {
+    "revision": "9e0aa556703592ea6e5c",
+    "url": "/react-clock-radio/static/js/runtime~main.ad908f40.js"
+  }
+]);
