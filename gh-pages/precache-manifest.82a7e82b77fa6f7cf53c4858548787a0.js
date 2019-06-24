68e77a85450df74a4a23b37d0138a46fb210409b
Jack <jamieson.jack@gmail.com>
2019-06-23 21:44:18 -0400

    deploy to gh-pages

---
+++ gh-pages/precache-manifest.82a7e82b77fa6f7cf53c4858548787a0.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "01f34df3077eef7ad32e3e332dfd0b36",
+    "url": "/index.html"
+  },
+  {
+    "revision": "d62772ec3bbb2aaa8ad0",
+    "url": "/static/css/main.96ab95a3.chunk.css"
+  },
+  {
+    "revision": "08731324183367807ea9",
+    "url": "/static/js/2.267e8e58.chunk.js"
+  },
+  {
+    "revision": "d62772ec3bbb2aaa8ad0",
+    "url": "/static/js/main.7d6ecaa8.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
